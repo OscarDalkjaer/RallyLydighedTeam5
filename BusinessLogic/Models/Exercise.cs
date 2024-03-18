6932ab151c50f57977b60e1d9dee2774fbc9880b
Oscar <oscardalkjaer@hotmail.com>
2024-03-18 13:42:59 +0100

    ExerciseStarted

---
+++ BusinessLogic/Models/Exercise.cs
@@ -0,0 +1,18 @@
+﻿namespace BusinessLogic.Models
+{
+    public class Exercise
+    {
+        public Exercise (int exerciseId, int number, LevelEnum level, TypeEnum type)
+        {
+            ExerciseId = exerciseId;
+            Number = number;
+            Level = level;
+            Type = type;
+        }
+
+        public int ExerciseId { get; private set; }
+        public int Number { get; private set; }
+        public LevelEnum Level { get; private set; }
+        public TypeEnum Type { get; private set; }
+    }
+}
