0d201499606658af555f1c76578f56dbd436ba8c
Oscar Dalkjær Wigant <odwi54124@edu.ucl.dk>
2024-04-22 12:41:39 +0200

    JudgeModelCleaned

--- BusinessLogic/Models/Judge.cs
+++ BusinessLogic/Models/Judge.cs
@@ -2,17 +2,17 @@
 {
     public class Judge
     {
+        public int? JudgeId { get; protected set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
 
         protected Judge() { }
+
         public Judge(string firstName, string lastName, int? judgeId = null) 
         {
             FirstName = firstName;
             LastName = lastName;
             JudgeId = judgeId;
-        }
-        public int? JudgeId { get; protected set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-
+        }      
     }
 }
