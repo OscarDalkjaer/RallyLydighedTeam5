7a3d6deb4c02d2fb4d3b68a00983612995d21979
Kim Raaschou Nielsen <kim.nielsen.dk@gmail.com>
2024-04-23 10:03:05 +0200

    format

--- DataAccess/Repositories/CourseTestRepository.cs
+++ DataAccess/Repositories/CourseTestRepository.cs
@@ -1,54 +1,51 @@
 ﻿using BusinessLogic.Models;
 using BusinessLogic.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DataAccess.Repositories
+namespace DataAccess.Repositories;
+
+public class CourseTestRepository : ICourseRepository
 {
-    public class CourseTestRepository : ICourseRepository
+    public List<Course> TestCourses { get; } = new List<Course>();
 
+    public async Task AddCourse(Course course)
     {
-        public List<Course> TestCourses { get; } = new List<Course>();
+        course.CourseId = TestCourses.Count + 1;
+        TestCourses.Add(course);
+        await Task.CompletedTask;
+    }
+
+    public async Task<Course?> GetCourse(int courseId)
+    {
+        Course? course = TestCourses.SingleOrDefault(c => c.CourseId == courseId);
+        return await Task.FromResult(course);
+    }
+
+    public async Task<IEnumerable<Course>> GetAllCourses()
+    {
+        return await Task.FromResult(TestCourses);
+    }
+
+    public async Task UpdateCourse(Course course)
+    {
+        Course? courseToUpdate = TestCourses.SingleOrDefault(c => c.CourseId == course.CourseId);
+
+        if (courseToUpdate != null)
+        {
+            courseToUpdate.Level = course.Level;
+        }
+
+        await Task.CompletedTask;
+    }
+
+    public async Task DeleteCourse(int courseId)
+    {
+        Course? course = TestCourses.SingleOrDefault(c => c.CourseId == courseId);
         
-
-        public async Task AddCourse(Course course)
+        if (course != null)
         {
-            int courseId = TestCourses.Count + 1;
-            course.CourseId = courseId;
-            TestCourses.Add(course);
-            await Task.CompletedTask;
-        }
-
-        public async Task<Course> GetCourse(int courseId)
-        {
-            Course? course = TestCourses.SingleOrDefault(c => c.CourseId == courseId);
-            return await Task.FromResult(course);
-
-        }
-
-         public async Task<IEnumerable<Course>> GetAllCourses()
-        {
-            return TestCourses;
-        }
-
-        public async Task UpdateCourse (Course course)
-        {
-            Course? courseToUpdate = TestCourses.SingleOrDefault(c => c.CourseId == course.CourseId);
-                if (courseToUpdate != null) 
-            {
-                courseToUpdate.Level = course.Level;
-            }
-            await Task.CompletedTask;
-        }
-
-        public async Task DeleteCourse(int courseId)
-        {
-            Course? course = TestCourses.FirstOrDefault(c => c.CourseId == courseId);
             TestCourses.Remove(course);
         }
+
+        await Task.CompletedTask;
     }
 }
