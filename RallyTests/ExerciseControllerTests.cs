f5b8302ff286b373d345d8e8873fe7f2152720b3
Lyanne Fernandez <lbfe44184@edu.ucl.dk>
2024-04-23 10:27:55 +0200

    rettelser til exercisecontroller test

--- RallyTests/ExerciseControllerTests.cs
+++ RallyTests/ExerciseControllerTests.cs
@@ -1,114 +1,108 @@
 ﻿using API.Controllers;
-using DataAccess.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using BusinessLogic.Models;
-using BusinessLogic.Services;
-using BusinessLogic;
 using DataAccess;
 using API.ViewModels;
 
-namespace RallyTests
+namespace RallyTests;
+
+[TestClass]
+public class ExerciseControllerTests
 {
-    [TestClass]
-    public class ExerciseControllerTests
+    [TestMethod]
+    public async Task TestAddExercise()
     {
-        [TestMethod]
-        public async Task TestAddExercise()
-        {
-            //Arrange
-            ExerciseTestRepository testRepository = new ExerciseTestRepository();
-            ExerciseController exerciseController = new ExerciseController(testRepository);
-            AddExerciseViewModel addExerciseViewModel = new AddExerciseViewModel(1, TypeEnum.Cone);
+        //Arrange
+        ExerciseTestRepository testRepository = new ExerciseTestRepository();
+        ExerciseController exerciseController = new ExerciseController(testRepository);
+        AddExerciseViewModel addExerciseViewModel = new AddExerciseViewModel(1, TypeEnum.Cone);
 
 
-            //Act
-            exerciseController.AddExercise(addExerciseViewModel);
+        //Act
+        await exerciseController.AddExercise(addExerciseViewModel);
 
 
-            //Assert
-            Assert.AreEqual(testRepository.exercises.Count(), 1);
-        }
+        //Assert
+        Assert.AreEqual(1, testRepository.TestExercises.Count);
+           
+    }
 
-        [TestMethod]
-        public async Task TestGetExercise()
-        {
-            //Arrange
-            ExerciseTestRepository testRepository = new ExerciseTestRepository();
-            ExerciseController exerciseController = new ExerciseController(testRepository);
-            AddExerciseViewModel addExerciseViewModel = new AddExerciseViewModel(1, TypeEnum.Cone);
-            await exerciseController.AddExercise(addExerciseViewModel);
+    [TestMethod]
+    public async Task TestGetExercise()
+    {
+        //Arrange
+        ExerciseTestRepository testRepository = new ExerciseTestRepository();
+        ExerciseController exerciseController = new ExerciseController(testRepository);
+        AddExerciseViewModel addExerciseViewModel = new AddExerciseViewModel(1, TypeEnum.Cone);
+        await exerciseController.AddExercise(addExerciseViewModel);
 
-            //Act
-            GetExerciseViewModel exerciseVM = await exerciseController.GetExercise(1);
+        //Act
 
-
-            //Assert
-            Assert.AreEqual(exerciseVM.GetExerciseId, 1);
-            Assert.AreEqual(exerciseVM.Type, TypeEnum.Cone);
-
-        }
-
-        [TestMethod]
-        public async Task TestGetAllExercises()
-        {
-            //Arrange
-            ExerciseTestRepository testRepository = new ExerciseTestRepository();
-            ExerciseController exerciseController = new ExerciseController(testRepository);
-            AddExerciseViewModel addExerciseViewModel = new AddExerciseViewModel(1, TypeEnum.Cone);
-            AddExerciseViewModel addExerciseViewModel1 = new AddExerciseViewModel(2, TypeEnum.Cone);
-            await exerciseController.AddExercise(addExerciseViewModel);
-            await exerciseController.AddExercise(addExerciseViewModel1);
-
-            //Act
-            IEnumerable<GetExerciseViewModel> exerciseVMs = await exerciseController.GetAllExercises();
-            int count = exerciseVMs.Count();
+       
+        GetExerciseViewModel exerciseVM = await exerciseController.GetExercise(1);
 
 
-            //Assert
-            Assert.AreEqual(count, 2);
+        //Assert
+        Assert.AreEqual(exerciseVM.GetExerciseId, 1);
+        Assert.AreEqual(exerciseVM.Type, TypeEnum.Cone);
 
-        }
+    }
 
-        [TestMethod]
-        public async Task TestUpdateExercise()
-        {
-            //Arrange
-            ExerciseTestRepository testRepository = new ExerciseTestRepository();
-            ExerciseController exerciseController = new ExerciseController(testRepository);
-            AddExerciseViewModel addExerciseViewModel = new AddExerciseViewModel(1, TypeEnum.Cone);            
-            await exerciseController.AddExercise(addExerciseViewModel);
-            UpdateExerciseViewModel updatedVM = new UpdateExerciseViewModel(2, TypeEnum.Standard, 1);
+    [TestMethod]
+    public async Task TestGetAllExercises()
+    {
+        //Arrange
+        ExerciseTestRepository testRepository = new ExerciseTestRepository();
+        ExerciseController exerciseController = new ExerciseController(testRepository);
+        AddExerciseViewModel addExerciseViewModel = new AddExerciseViewModel(1, TypeEnum.Cone);
+        AddExerciseViewModel addExerciseViewModel1 = new AddExerciseViewModel(2, TypeEnum.Cone);
+        await exerciseController.AddExercise(addExerciseViewModel);
+        await exerciseController.AddExercise(addExerciseViewModel1);
 
-            //Act
-            await exerciseController.UpdateExercise(updatedVM);
+        //Act
+        IEnumerable<GetExerciseViewModel> exerciseVMs = await exerciseController.GetAllExercises();
+        int count = exerciseVMs.Count();
 
-            //Assert
-            Assert.AreEqual(testRepository.exercises[0].Number, 2);
-            Assert.AreEqual(testRepository.exercises[0].ExerciseId, 1);
-            Assert.AreEqual(testRepository.exercises[0].Type, TypeEnum.Standard);
-            
-        }
 
-        [TestMethod]
-        public async Task TestDeleteExercise() 
-        {
-            //Arrange
-            ExerciseTestRepository testRepository = new ExerciseTestRepository();
-            ExerciseController exerciseController = new ExerciseController(testRepository);
-            AddExerciseViewModel addExerciseViewModel = new AddExerciseViewModel(1, TypeEnum.Cone);
-            await exerciseController.AddExercise(addExerciseViewModel);
+        //Assert
+        Assert.AreEqual(count, 2);
 
-            //Act
-            await exerciseController.DeleteExercise(1);
-            int count = testRepository.exercises.Count();
+    }
 
-            //Assert
-            Assert.AreEqual(count, 0);
+    [TestMethod]
+    public async Task TestUpdateExercise()
+    {
+        //Arrange
+        ExerciseTestRepository testRepository = new ExerciseTestRepository();
+        ExerciseController exerciseController = new ExerciseController(testRepository);
+        AddExerciseViewModel addExerciseViewModel = new AddExerciseViewModel(1, TypeEnum.Cone);            
+        await exerciseController.AddExercise(addExerciseViewModel);
+        UpdateExerciseViewModel updatedVM = new UpdateExerciseViewModel(2, TypeEnum.Standard, 1);
 
-        }
+        //Act
+        await exerciseController.UpdateExercise(updatedVM);
+
+        //Assert
+        Assert.AreEqual(testRepository.exercises[0].Number, 2);
+        Assert.AreEqual(testRepository.exercises[0].ExerciseId, 1);
+        Assert.AreEqual(testRepository.exercises[0].Type, TypeEnum.Standard);
+        
+    }
+
+    [TestMethod]
+    public async Task TestDeleteExercise() 
+    {
+        //Arrange
+        ExerciseTestRepository testRepository = new ExerciseTestRepository();
+        ExerciseController exerciseController = new ExerciseController(testRepository);
+        AddExerciseViewModel addExerciseViewModel = new AddExerciseViewModel(1, TypeEnum.Cone);
+        await exerciseController.AddExercise(addExerciseViewModel);
+
+        //Act
+        await exerciseController.DeleteExercise(1);
+        int count = testRepository.exercises.Count();
+
+        //Assert
+        Assert.AreEqual(count, 0);
+
     }
 }
