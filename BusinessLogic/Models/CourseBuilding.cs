be04195ff7d99a1fec7b797a0d0aa20f83dee042
UllaRaaschou <126786969+UllaRaaschou@users.noreply.github.com>
2024-04-25 13:43:50 +0200

    CourseBuilding

---
+++ BusinessLogic/Models/CourseBuilding.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Models
+{
+    public class CourseBuilding
+    {
+        public CourseBuilding(Course course, Event? @event, Judge? judge) 
+        {
+            Course = course;
+            Event = @event;
+            Judge = judge;
+            PossibleExercises = new List<Exercise>();
+            ChosenExercises = new List<Exercise>();
+
+        }
+
+        public Course Course { get; }
+        public Event? Event { get; }
+        public Judge? Judge { get; }
+        public List<Exercise> PossibleExercises { get; }
+        public List<Exercise> ChosenExercises { get; }  
+    }
+}
