2500b5857a563ef75cf859740c809ab0c6282275
UllaRaaschou <126786969+UllaRaaschou@users.noreply.github.com>
2024-05-09 12:42:53 +0200

    TestGetMinLengthOfExerciseLis

---
+++ RallyTests/CourseClassTests.cs
@@ -0,0 +1,42 @@
+﻿using BusinessLogic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RallyTests
+{
+    [TestClass]
+    public class CourseClassTests
+    {
+        [TestMethod]
+        public async Task TestGetMinLengthOfExerciseList()
+        {
+            //Assert
+            Course course = new Course(LevelEnum.Champion);
+
+            //Act
+            int max = course.GetMinLengthOfExerciseList(course.Level);
+
+            //Assert
+            Assert.AreEqual(18, max);
+        }
+
+
+
+
+        [TestMethod]
+        public async Task TestGetMaxLengthOfExerciseList() 
+        {
+            //Assert
+            Course course = new Course(LevelEnum.Champion);
+
+            //Act
+            int max = course.GetMaxLengthOfExerciseList(course.Level);
+
+            //Assert
+            Assert.AreEqual(20, max);
+        }
+    }
+}
