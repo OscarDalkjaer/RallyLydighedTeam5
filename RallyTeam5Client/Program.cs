a4fd91ece2c6f1686f09e008f96ea7159cc5fa66
Kim Raaschou Nielsen <kim.nielsen.dk@gmail.com>
2024-05-21 20:42:46 +0200

    added session cookie auth

--- RallyTeam5Client/Program.cs
+++ RallyTeam5Client/Program.cs
@@ -1,10 +1,31 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using RallyTeam5Client;
 using RallyTeam5Client.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddRazorPages();
+
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+builder.Services.AddCascadingAuthenticationState();
+
+builder.Services.AddHttpClient<ApiEndpoints>(httpClient =>
+{
+    httpClient.BaseAddress = new Uri("https://localhost:7288");
+    httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+});
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services
+    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/login";
+        options.LogoutPath = "/logout";
+    });
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -16,12 +37,15 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-
 app.UseStaticFiles();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.UseHttpsRedirection();
+
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
 
 app.Run();
