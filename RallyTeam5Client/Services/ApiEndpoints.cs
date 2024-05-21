54fb7eae4157325722459b0d0a077a809eb46ba1
Kim Raaschou Nielsen <kim.nielsen.dk@gmail.com>
2024-05-21 20:41:20 +0200

    throw when api login fails

---
+++ RallyTeam5Client/Services/ApiEndpoints.cs
@@ -0,0 +1,25 @@
+﻿namespace RallyTeam5Client;
+
+public class ApiEndpoints
+{
+    private record LoginRequest(string Email, string Password);
+    private record LoginResponse(string AccessToken);
+    private readonly HttpClient httpClient;
+
+    public ApiEndpoints(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
+    public async Task Login(string email, string password)
+    {
+        const string requestUri = "/login?useSessionCookies=true";
+        JsonContent jsonContent = JsonContent.Create(new LoginRequest(email, password));
+
+        HttpResponseMessage responseMessage = await httpClient.PostAsync(requestUri, jsonContent);
+        
+        if (!responseMessage.IsSuccessStatusCode)
+        {
+            throw new Exception("Login to Server API failed");
+        }
+    }
+}
