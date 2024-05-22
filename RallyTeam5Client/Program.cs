37839bc4e5c50b83f218aa667eda68caf3dda54a
Kim Raaschou Nielsen <kim.nielsen.dk@gmail.com>
2024-05-22 21:10:29 +0200

    added CookieAuthenticationStateProvider

--- RallyTeam5Client/Program.cs
+++ RallyTeam5Client/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using RallyTeam5Client.Services;
@@ -12,12 +13,16 @@ namespace RallyTeam5Client
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddHttpClient<RallyTeam5HttpClient, RallyTeam5HttpClient>()
-                .ConfigureHttpClient(httpClient => {
-                    httpClient.BaseAddress = new Uri("https://localhost:7288");// builder.HostEnvironment.BaseAddress);
-                    httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
-                    // httpClient.DefaultRequestHeaders.Add("Access-Control-Allow-Origin", "https://localhost:7288");
-                });
+            // builder.Services.AddHttpClient<RallyTeam5HttpClient>()
+            //     .ConfigureHttpClient(httpClient => {
+            //         httpClient.BaseAddress = new Uri("https://localhost:7288");// builder.HostEnvironment.BaseAddress);
+            //         httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+            //         // httpClient.DefaultRequestHeaders.Add("Access-Control-Allow-Origin", "https://localhost:7288");
+            //     });
+
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7288/") });
+            builder.Services.AddScoped<AuthenticationStateProvider, CookieAuthenticationStateProvider>();
                 
             await builder.Build().RunAsync();
         }
