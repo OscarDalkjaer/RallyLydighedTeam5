3e68821aea4f427a9abbc6a7068edb8e50491782
Kim Raaschou Nielsen <kim.nielsen.dk@gmail.com>
2024-05-23 20:41:33 +0200

    new migration add ef identity users

--- DataAccess/DbContext/CourseContext.cs
+++ DataAccess/DbContext/CourseContext.cs
@@ -1,5 +1,4 @@
-﻿using BusinessLogic.Models;
-using DataAccess.DataAccessModels;
+﻿using DataAccess.DataAccessModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +10,25 @@ public class CourseContext : IdentityDbContext<IdentityUser>
     public CourseContext(DbContextOptions<CourseContext> context) : base(context)
     {
     }
+
+    // Kim: update docker database på mac. 
+
+    public CourseContext()
+    {
+
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // docker run --cap-add SYS_PTRACE -e 'ACCEPT_EULA=1' -e 'MSSQL_SA_PASSWORD=Passw0rd' -p 1433:1433 --name azuresqledge -d mcr.microsoft.com/azure-sql-edge
+
+        // dotnet ef database update --project DataAccess
+        // dotnet ef migrations add Initial --project DataAccess
+        // dotnet ef database update --project DataAccess
+
+        optionsBuilder.UseSqlServer("Server=localhost;Database=RallyTeam5DB;User Id=sa;Password=Passw0rd;TrustServerCertificate=True;");
+        base.OnConfiguring(optionsBuilder);
+    }
+
     public DbSet<JudgeDataAccessModel> JudgeDataAccessModels { get; set; }
     public DbSet<EventDataAccessModel> EventDataAccessModels { get; set; }
     public DbSet<ExerciseDataAccessModel> ExerciseDataAccessModels { get; set; }
@@ -28,20 +46,59 @@ public class CourseContext : IdentityDbContext<IdentityUser>
         modelBuilder.Entity<ExerciseDataAccessModel>()
             .HasData(Seeder.ExerciseDataAccessModels);
 
-        modelBuilder.Entity<JudgeDataAccessModel>()
-            .HasData(
+        modelBuilder.Entity<JudgeDataAccessModel>().HasData(
             new JudgeDataAccessModel("Peter", "Madsen", 1),
             new JudgeDataAccessModel("Minna", "Mogensen", 2),
-            new JudgeDataAccessModel("Thilde", "Thrane", 3)
-            );
+            new JudgeDataAccessModel("Thilde", "Thrane", 3));
 
-        modelBuilder.Entity<EventDataAccessModel>()
-            .HasData(
+        modelBuilder.Entity<EventDataAccessModel>().HasData(
             new EventDataAccessModel("Odense RallyEvent", new DateTime(2024, 08, 08), "5000 Odense", 1),
             new EventDataAccessModel("Billund Rally-Cup", new DateTime(2024, 01, 09), "7190 Billund", 2),
-            new EventDataAccessModel("Roskilde Rally", new DateTime(2025, 02, 04), "4000 Roskilde", 3)
-            );
+            new EventDataAccessModel("Roskilde Rally", new DateTime(2025, 02, 04), "4000 Roskilde", 3));
+
+        SeedEntityFrameworkIdentityUsewrsAndRoles(modelBuilder);
 
         base.OnModelCreating(modelBuilder);
+    }
+
+    private static void SeedEntityFrameworkIdentityUsewrsAndRoles(ModelBuilder modelBuilder)
+    {
+        string userIdForUlla = "9d8b0a60-e3b1-4088-9ff5-6b0a68d80cac";
+        string userIdForOscar = "1ca1abc7-24b8-4d66-a7b1-b21adc12101c";
+        string userIdForLyanne = "f47c5bf1-740c-4fb9-94b7-941e90ad7d23";
+
+        PasswordHasher<IdentityUser> hasher = new PasswordHasher<IdentityUser>();
+
+        modelBuilder.Entity<IdentityUser>().HasData(
+            new IdentityUser
+            {
+                Id = userIdForUlla,
+                UserName = "ulla@test.com",
+                NormalizedUserName = "ulla@test.com".ToUpper(),
+                Email = "ulla@test.com",
+                NormalizedEmail = "ulla@test.com".ToUpper(),
+                EmailConfirmed = true,
+                PasswordHash = hasher.HashPassword(null!, "Pa$$w0rd")
+            },
+            new IdentityUser
+            {
+                Id = userIdForOscar,
+                UserName = "oscar",
+                NormalizedUserName = "oscar".ToUpper(),
+                Email = "oscar@test.com",
+                NormalizedEmail = "oscar@test.com".ToUpper(),
+                EmailConfirmed = true,
+                PasswordHash = hasher.HashPassword(null!, "Pa$$w0rd")
+            },
+            new IdentityUser
+            {
+                Id = userIdForLyanne,
+                UserName = "lyanne",
+                NormalizedUserName = "lyanne".ToUpper(),
+                Email = "lyanne@test.com",
+                NormalizedEmail = "lyanne@test.com".ToUpper(),
+                EmailConfirmed = true,
+                PasswordHash = hasher.HashPassword(null!, "Pa$$w0rd")
+            });
     }
 }
