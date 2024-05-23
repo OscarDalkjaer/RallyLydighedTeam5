64fe2cf08a7626f5eb431ece9747e19f12b5df7d
Kim Raaschou Nielsen <kim.nielsen.dk@gmail.com>
2024-05-23 12:27:16 +0200

    login and auth

--- RallyTeam5Client/Program.cs
+++ RallyTeam5Client/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -11,8 +12,14 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri("https://localhost:7288/") });
 
-builder.Services.AddScoped<AuthenticationStateProvider, ServerApiCookieAuthenticationStateProvider>();
-builder.Services.AddScoped(sp => (IUserManager)sp.GetRequiredService<AuthenticationStateProvider>());
+builder.Services.AddScoped<ServerApiCookieAuthenticationStateProvider>();
+
+builder.Services.AddScoped<AuthenticationStateProvider>(sp => sp.GetRequiredService<ServerApiCookieAuthenticationStateProvider>());
+builder.Services.AddScoped<IUserManager>(sp => sp.GetRequiredService<ServerApiCookieAuthenticationStateProvider>());
+
 builder.Services.AddScoped<ApiEndpoints>();
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
